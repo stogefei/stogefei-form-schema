8301733860bb07f5e833f82bd6de331ef6609643
zhuaofei <zhuafei@authine.com>
2022-10-28 11:57:53 +0800

    feat: 🎸 add
    add
    
    BREAKING CHANGE: 🧨 add

--- packages/form-core/babel.config.js
+++ packages/form-core/babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
+  compact: false,
+  presets: [
+    '@babel/preset-env',
+    [
+      '@vue/app',
+      {
+        corejs: 3,
+        targets: {
+          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        absoluteRuntime: false,
+        corejs: 3,
+      },
+    ],
+    [
+      'import',
+      { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true },
+      'ant-design-vue',
+    ],
+  ],
 };
--- packages/form-core/src/main.ts
+++ packages/form-core/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import App2 from './App.vue';
 
 createApp(App).mount('#app');

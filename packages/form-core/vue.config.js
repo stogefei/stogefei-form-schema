12ed42bc6f29b8b08a8cf109450415643f3373c1
zhuaofei <zhuafei@authine.com>
2022-11-02 10:40:11 +0800

    improvement(custom): 打包优化

--- packages/form-core/vue.config.js
+++ packages/form-core/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const CircularPlugin = require('circular-dependency-plugin');
+const env = process.env.mode === 'production';
 const lang = new MonacoWebpackPlugin();
 const circular = new CircularPlugin();
 module.exports = defineConfig({
@@ -15,7 +16,9 @@ module.exports = defineConfig({
     },
   },
   chainWebpack (config) {
-    config.plugin('monaco').use(lang);
-    config.plugin('circular').use(circular);
+    if (!env) {
+      config.plugin('monaco').use(lang);
+      config.plugin('circular').use(circular);
+    }
   },
 });
